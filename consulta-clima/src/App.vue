a203352cfe9c50f34d16749671b0ff6c6ed2e0e3
Hector Ibarra <35663477+Hayo412@users.noreply.github.com>
2020-06-06 00:41:25 -0500

    adding API call

--- consulta-clima/src/App.vue
+++ consulta-clima/src/App.vue
@@ -2,12 +2,18 @@
   <div id="app">
     <main>
       <div class="search-box">
-        <input type="text" class="search-bar" placeholder="Ciudad de Origen" />
+        <input
+          type="text"
+          class="search-bar"
+          placeholder="Ciudad de Origen"
+          v-model="query"
+          @keypress="fetchWeather"
+        />
       </div>
 
-      <div class="weather-wrap">
+      <div class="weather-wrap" v-if="typeof weather.main != 'undefined'">
         <div class="location-box">
-          <div class="location">Seoul, KR</div>
+          <div class="location">{{weather.name}}, {{weather.sys.country}}</div>
           <div class="date">Monday 20 January 2020</div>
         </div>
 
@@ -25,8 +31,27 @@ export default {
   name: "app",
   data() {
     return {
-      api_key: "3bfa022c0c1f06dc24f33f2fadc18cf5"
+      api_key: "3bfa022c0c1f06dc24f33f2fadc18cf5",
+      url_base: "https://api.openweathermap.org/data/2.5/",
+      query: "",
+      weather: {}
     };
+  },
+  methods: {
+    fetchWeather(e) {
+      if (e.key == "Enter") {
+        fetch(
+          `${this.url_base}weather?q=${this.query}&units=metric&APPID=${this.api_key}`
+        )
+          .then(res => {
+            return res.json();
+          })
+          .then(this.setResults);
+      }
+    },
+    setResults(results) {
+      this.weather = results;
+    }
   }
 };
 </script>
